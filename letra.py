e75782b28b9933372e53e48ac9117f26bcb68c41
Elena <38448479+LigiaPerez12@users.noreply.github.com>
2019-05-10 08:50:38 -0500

    Add files via upload

---
+++ letra.py
@@ -0,0 +1,21 @@
+import turtle
+t= turtle.Pen()
+t.forward(60)
+t.left (-90)
+t.forward(200)
+t.left (45)
+t.left (45)
+t.forward(100)
+t.left (-45)
+t.left (-45)
+t.forward(55)
+t.left(-90)
+t.forward (160)
+t.left (-90)
+t.forward (255)
+
+
+
+
+
+turtle.getscreen()._root.mainloop()
